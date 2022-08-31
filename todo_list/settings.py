b40cb5cf604150eec9e03c36ab61c3a50dca8d20
Dullah Mtavya <mtavya420@gmail.com>
2022-08-31 19:00:48 +0900

    make it better

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -125,7 +125,7 @@ LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
